24a353fb10ef14334486f37cab6f6fa0b016c4b3
holtiwilan <tim.holzhausen@web.de>
2018-04-17 22:47:37 +0200

    update

--- scripts/phone_detect.sh
+++ scripts/phone_detect.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Seppys iPhone
-sudo hping3 -2 -c 10 -p 5353 -i u1 192.168.178.44 -q >/dev/null 2>&1
-sudo hping3 -2 -c 10 -p 5353 -i u1 192.168.178.30 -q >/dev/null 2>&1
+sudo hping3 -2 -c 10 -p 5353 -i u1 192.168.178.60 -q >/dev/null 2>&1
+sudo hping3 -2 -c 10 -p 5353 -i u1 192.168.178.83 -q >/dev/null 2>&1
 DEVICES=`sudo arp -an | awk '{print $4}'`
-if [[ $DEVICES == *d4:f4:6f:8d:b3:f5* ]]
+if [[ $DEVICES == *00:EC:0A:70:F7:2E* ]]
   then
     echo "Tim anwesend"
     curl --max-time 2 --connect-timeout 2 --header "Content-Type: text/plain" --request POST --data "ON" http://openhabian:8080/rest/items/PresenceTim
@@ -12,7 +12,7 @@ if [[ $DEVICES == *d4:f4:6f:8d:b3:f5* ]]
     echo "Tim abwesend"
     curl --max-time 2 --connect-timeout 2 --header "Content-Type: text/plain" --request POST --data "OFF" http://openhabian:8080/rest/items/PresenceTim
 fi
-if [[ $DEVICES == *00:42:37:57:08:29* ]]
+if [[ $DEVICES == *E4:46:DA:6C:1C:44* ]]
   then
     echo "Sandra anwesend"
     curl --max-time 2 --connect-timeout 2 --header "Content-Type: text/plain" --request POST --data "ON" http://openhabian:8080/rest/items/PresenceSandra
